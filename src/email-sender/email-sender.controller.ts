ffffa0f5afca2c246f5bdd48250fac5229ba499b
Diego Barrueta <diego.barrueta@gmail.com>
2024-12-28 11:33:43 -0300

    cors

--- src/email-sender/email-sender.controller.ts
+++ src/email-sender/email-sender.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { EmailSenderService } from './email-sender.service';
+import { Response } from 'express';
 
 @Controller('email-sender')
 export class EmailSenderController {
   constructor(private readonly emailSenderService: EmailSenderService) {}
 
   @Post()
-  create(@Body() data: any) {
+  create(@Body() data: any, @Res() res: Response) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     console.log('🚀 ~ EmailSenderController ~ create ~ data:', data);
     return this.emailSenderService.send(data);
   }
